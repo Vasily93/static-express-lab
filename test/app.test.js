906b1f005a531075d458803c9a604136bd01177f
Vasily93 <markovavasily@gmail.com>
2019-07-11 10:07:23 -0700

    fetched list of colors on home page

--- test/app.test.js
+++ test/app.test.js
@@ -6,13 +6,21 @@ describe('app routes', () => {
     return request(app)
       .get('/')
       .then(res => {
-        expect(res.text).toEqual(expect.stringContaining('hi'));
+        expect(res.text).toEqual(expect.stringContaining('Colors'));
       });
   });
 
   // it('returns  array of colors', () => {
   //   return request(app)
   //     .get('/api/v1/colors')
+  //     .then(res => {
+  //       expect(res.text).toEqual(expect.Array());
+  //     });
+  // });
+
+  // it('returns a color by name', () => {
+  //   return request(app)
+  //     .get('/api/v1/colors/:name')
   //     .then(res => {
   //       expect(res.text).toEqual(expect.Array());
   //     });
